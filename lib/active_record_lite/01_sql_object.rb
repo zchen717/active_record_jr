7cdeed71bd6f1c083b6952951ac8a908b48323f0
Zach Chen <zachen717@gmail.com>
2014-08-22 15:37:26 -0700

    Finished up until part 1f.

--- lib/active_record_lite/01_sql_object.rb
+++ lib/active_record_lite/01_sql_object.rb
@@ -11,6 +11,7 @@ class SQLObject
         *
       FROM
         "#{self.table_name}"
+        
     SQL
     col = col.first
     @columns = col.map! { |el| el.to_sym }
@@ -39,33 +40,48 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+    SQL
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |data| self.new(data) }
   end
 
   def self.find(id)
-    # ...
+    results = DBConnection.execute(<<-SQL, id)
+      SELECT
+      #{self.table_name}.*
+      FROM
+      #{self.table_name}
+      WHERE
+      #{self.table_name}.id = ?
+    SQL
+    parse_all(results).first
   end
 
   def attributes
     @attributes ||= {}
-    # ...
   end
 
   def insert
-    # ...
+
   end
 
-  def initialize()
-    # params.each do |attr_name, value|
-    #   attr_name = attr_name.to_sym
-    #   if attribues.include?(attr_name)
-    #     raise "unknown attribute '#{value}'"
-    #   end
-    # end
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def save
